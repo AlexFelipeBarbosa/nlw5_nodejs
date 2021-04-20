8593e0bfd6ce170acedab045e27cbdeb578885b3
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-04-20 09:22:49 -0300

    Aula 1

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ mensagem: "Olá!!!" });
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Usuário salvo com sucesso!NLW" });
+});
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
